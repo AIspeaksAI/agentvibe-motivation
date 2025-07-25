b969512ce987583bbe58b66cc5826b18f1ff066c
antoineissaly <antoine.issaly.pro@gmail.com>
2025-07-25 17:15:38 -0400

    very sad confetti

--- force-app/main/default/lwc/motivationButton/motivationButton.js
+++ force-app/main/default/lwc/motivationButton/motivationButton.js
@@ -4,14 +4,20 @@ import getMotivationMessage from '@salesforce/apex/MotivationController.getMotiv
 export default class MotivationButton extends LightningElement {
     @track message = '';
     @track isLoading = false;
+    @track showConfetti = false;
+    @track confettiPieces = [];
 
     /**
      * Handles the motivation button click event
      * Calls the Apex controller to get a random motivational message
+     * Triggers confetti animation
      */
     handleMotivationClick() {
         this.isLoading = true;
         this.message = ''; // Clear previous message
+        
+        // Trigger confetti animation
+        this.triggerConfetti();
         
         getMotivationMessage()
             .then(result => {
@@ -23,5 +29,30 @@ export default class MotivationButton extends LightningElement {
                 this.message = 'Something went wrong, but remember: you\'ve got this! 💪';
                 this.isLoading = false;
             });
+    }
+
+    /**
+     * Creates and triggers confetti animation
+     */
+    triggerConfetti() {
+        this.showConfetti = true;
+        this.confettiPieces = [];
+        
+        // Create confetti pieces
+        const colors = ['#ff6b6b', '#4ecdc4', '#45b7d1', '#96ceb4', '#feca57', '#ff9ff3', '#54a0ff'];
+        const confettiCount = 50;
+        
+        for (let i = 0; i < confettiCount; i++) {
+            this.confettiPieces.push({
+                id: i,
+                style: `left: ${Math.random() * 100}%; animation-delay: ${Math.random() * 2}s; background-color: ${colors[Math.floor(Math.random() * colors.length)]};`
+            });
+        }
+        
+        // Hide confetti after animation completes
+        setTimeout(() => {
+            this.showConfetti = false;
+            this.confettiPieces = [];
+        }, 3000);
     }
 } 
